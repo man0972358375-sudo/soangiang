d090431a6018b2eb083d2ebd6d0fcd4d85cbfd22
man0972358375-sudo <man0972358375@gmail.com>
2025-12-10 10:43:01 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Nguyễnn Thị Mận - Trường Tiểu học Hồng Lạc - ĐT: 097 2358 375</p>
+    <p>Tác giả: Nguyễn Thị Mận - Trường Tiểu học Hồng Lạc - ĐT: 097 2358 375</p>
 </div>
 """, unsafe_allow_html=True)
 
